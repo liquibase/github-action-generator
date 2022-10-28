02acdd0b4c560c1d03c2d20040e3899daf7535a0
Derek Smart <derek@grindaga.com>
2022-10-28 11:22:03 -0400

    move create issue func to target script file
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- scripts/push-to-repository.sh
+++ scripts/push-to-repository.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-source create-issue.sh
-
 COMMAND="${1/ /-}" #replace spaces with dashes
 TAG=$2
 REPO="https://liquibot:$BOT_TOKEN@github.com/liquibase-github-actions/$COMMAND.git"
@@ -14,6 +12,21 @@ if [[ -z "$BOT_TOKEN" ]]; then
   echo "Set the BOT_TOKEN env variable."
 	exit 1
 fi
+
+create_issue() {
+  local COMMAND=$1
+  local REPO="liquibase/github-action-generator"
+  local TITLE="New Action Created: $COMMAND"
+  local BODY="A new action repository has been created for $COMMAND. Please release the first version to the marketplace by creating a [new release](https://github.com/liquibase-github-actions/$COMMAND/releases) and publishing to the marketplace."
+
+  curl \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $BOT_TOKEN" \
+    https://api.github.com/repos/$REPO/issues \
+    -d "{\"title\":\"$TITLE\",\"body\":\"$BODY\",\"labels\":[\"enhancement\"]}"
+}
+
 
 # Set up target repository
 {
