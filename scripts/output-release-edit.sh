e67fec14b0f8580eebb83414af0575840e182bfc
Derek Smart <derek@grindaga.com>
2022-11-04 15:34:30 -0400

    better clean up commands.json
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- scripts/output-release-edit.sh
+++ scripts/output-release-edit.sh
@@ -4,8 +4,9 @@ set -e
 
 TAG=$1
 
-for row in $(jq -r '.[]' commands.json); do
-    COMMAND="${row/ /-}" #replace spaces with dashes
-    echo "https://github.com/liquibase-github-actions/$COMMAND/releases/edit/$TAG"
-done
+FILE=$(cat commands.json)
+COMMANDS="${FILE// /-}" #replace spaces with dashes
 
+for row in $(echo $COMMANDS | jq -r '.[]'); do
+    echo "https://github.com/liquibase-github-actions/$row/releases/edit/$TAG"
+done
