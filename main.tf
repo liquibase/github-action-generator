83d86856741b83571062a2f2740fb242a9a8fb6c
jandroav <jandroav@icloud.com>
2024-01-04 16:08:01 +0100

    chore(main.tf): add remote backend configuration for Terraform state management to store state in spacelift.io organization and workspace "liquibase-github-actions" feat(main.tf): update github_repository resource to use for_each to create multiple repositories based on local.commands, and improve resource configuration formatting for better readability

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = "1.5.7"
-
+  backend "remote" {
+    hostname     = "spacelift.io"
+    organization = "liquibase"
+    workspaces {
+      name = "liquibase-github-actions"
+    }
+  }
   required_providers {
     github = {
       source  = "integrations/github"
@@ -19,12 +25,12 @@ locals {
 }
 
 resource "github_repository" "liquibase-github-actions" {
-  for_each = toset(local.commands)
-  name            = replace(each.key, " ", "-")
-  description     = "Official GitHub Action to run Liquibase ${title(replace(each.key, "-", " "))}"
-  visibility      = "public"
-  has_downloads   = false
-  has_issues      = false
-  has_projects    = false
-  has_wiki        = false
+  for_each      = toset(local.commands)
+  name          = replace(each.key, " ", "-")
+  description   = "Official GitHub Action to run Liquibase ${title(replace(each.key, "-", " "))}"
+  visibility    = "public"
+  has_downloads = false
+  has_issues    = false
+  has_projects  = false
+  has_wiki      = false
 }
