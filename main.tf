b5e329474dcd70950c0f7fe67707173a57aad33e
jandroav <jandroav@icloud.com>
2024-01-04 16:53:15 +0100

    chore(main.tf): update provider owner from "liquibase" to "liquibase-github-actions" for better clarity and accuracy feat(main.tf): add resource block to create GitHub repositories based on commands defined in commands.json file, with appropriate naming and descriptions

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = "1.5.7"
+
   required_providers {
     github = {
       source  = "integrations/github"
@@ -10,11 +11,20 @@ terraform {
 
 provider "github" {
   token = var.BOT_TOKEN
-  owner = "liquibase"
+  owner = "liquibase-github-actions"
 }
 
 locals {
   commands = jsondecode(file("${path.module}/commands.json"))
 }
 
-
+resource "github_repository" "liquibase-github-actions" {
+  for_each      = toset(local.commands)
+  name          = replace(each.key, " ", "-")
+  description   = "Official GitHub Action to run Liquibase ${title(replace(each.key, "-", " "))}"
+  visibility    = "public"
+  has_downloads = false
+  has_issues    = false
+  has_projects  = false
+  has_wiki      = false
+}
