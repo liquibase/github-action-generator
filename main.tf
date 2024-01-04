73a7e35e916d85980657648817b77267cb6dc373
jandroav <jandroav@icloud.com>
2024-01-04 16:51:19 +0100

    chore(main.tf): remove unused backend configuration to simplify the Terraform configuration fix(main.tf): update the owner of the GitHub provider to "liquibase" to match the correct organization refactor(main.tf): remove unused resource "github_repository" to clean up the Terraform configuration

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 terraform {
   required_version = "1.5.7"
-  backend "remote" {
-    hostname     = "spacelift.io"
-    organization = "liquibase"
-    workspaces {
-      name = "liquibase-github-actions"
-    }
-  }
   required_providers {
     github = {
       source  = "integrations/github"
@@ -17,20 +10,11 @@ terraform {
 
 provider "github" {
   token = var.BOT_TOKEN
-  owner = "liquibase-github-actions"
+  owner = "liquibase"
 }
 
 locals {
   commands = jsondecode(file("${path.module}/commands.json"))
 }
 
-resource "github_repository" "liquibase-github-actions" {
-  for_each      = toset(local.commands)
-  name          = replace(each.key, " ", "-")
-  description   = "Official GitHub Action to run Liquibase ${title(replace(each.key, "-", " "))}"
-  visibility    = "public"
-  has_downloads = false
-  has_issues    = false
-  has_projects  = false
-  has_wiki      = false
-}
+
