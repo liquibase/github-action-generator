d21f3a9ca31d212f5f5d6379baa4f6437428c42b
Sayali M <sayali@Sayalis-MacBook-Pro>
2024-02-09 17:45:39 -0600

    mount file path issue

--- main.tf
+++ main.tf
@@ -20,13 +20,13 @@ output "commands_file_exists" {
   value = local.commands
 }
 
-# resource "github_repository" "liquibase-github-actions" {
-#   for_each      = toset(local.commands)
-#   name          = replace(each.key, " ", "-")
-#   description   = "Official GitHub Action to run Liquibase ${title(replace(each.key, "-", " "))}"
-#   visibility    = "public"
-#   has_downloads = false
-#   has_issues    = false
-#   has_projects  = false
-#   has_wiki      = false
-# }
+resource "github_repository" "liquibase-github-actions" {
+  for_each      = toset(local.commands)
+  name          = replace(each.key, " ", "-")
+  description   = "Official GitHub Action to run Liquibase ${title(replace(each.key, "-", " "))}"
+  visibility    = "public"
+  has_downloads = false
+  has_issues    = false
+  has_projects  = false
+  has_wiki      = false
+}
