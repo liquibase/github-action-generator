bd668d9b0c486168c6126e8f089a0351aeef4fad
Alejandro Alvarez <github@jandroav.net>
2025-07-31 09:08:47 +0200

    Refactor GitHub provider to use vault secrets module
    Remove unused variables and switch authentication to pull secrets from
    the devops_vault_secrets module.

--- main.tf
+++ main.tf
@@ -7,12 +7,18 @@ terraform {
   }
 }
 
+module "devops_vault_secrets" {
+  source      = "../modules/vault-secrets"
+  vault_type  = "devops"
+  secret_keys = ["LIQUIBASE_TERRAFORM_GH_APP_ID", "LIQUIBASE_TERRAFORM_GH_INSTALL_ID", "LIQUIBASE_TERRAFORM_GH_APP_PRIVATE_KEY"]
+}
+
 provider "github" {
   owner = "liquibase-github-actions"
   app_auth {
-    id              = var.LIQUIBASE_TERRAFORM_GH_APP_ID
-    installation_id = var.LIQUIBASE_TERRAFORM_GH_INSTALL_ID_ACTIONS
-    pem_file        = file(var.LIQUIBASE_TERRAFORM_GH_APP_PRIVATE_KEY)
+    id              = module.devops_vault_secrets.secrets["LIQUIBASE_TERRAFORM_GH_APP_ID"]
+    installation_id = module.devops_vault_secrets.secrets["LIQUIBASE_TERRAFORM_GH_INSTALL_ID"]
+    pem_file        = module.devops_vault_secrets.secrets["LIQUIBASE_TERRAFORM_GH_APP_PRIVATE_KEY"]
   }
   write_delay_ms = 200 # Performance tuning
 }
